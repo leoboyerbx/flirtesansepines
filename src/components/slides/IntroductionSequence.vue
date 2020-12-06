3bf0a768d39d75d80238b48076cf78172ec80d27
leoboyerbx <contact@leoboyer.fr>
2020-12-06 17:06:14 +0100

    💄 Layout fixes

--- src/components/slides/IntroductionSequence.vue
+++ src/components/slides/IntroductionSequence.vue
@@ -1,8 +1,8 @@
 <template>
   <article class="experience-introduction" :class="currentState">
     <div class="container">
-        <img src="../../assets/rose.svg">
-        <h1>{{ title }}</h1>
+      <img src="../../assets/rose.svg">
+      <h1>{{ title }}</h1>
     </div>
   </article>
 </template>
@@ -10,10 +10,10 @@
 <script>
 export default {
   name: 'IntroductionSequence',
-    data: () => ({
-        title: "Flirte sans épines"
-    }),
-   props: {
+  data: () => ({
+    title: "Flirte sans épines"
+  }),
+  props: {
     currentState: {
       type: String,
       default: 'future'
@@ -26,41 +26,43 @@ export default {
 <style scoped lang="scss">
 
 .experience-introduction {
-    display:none;
+  display:none;
+  &.current {
+    display: block;
+  }
 
-    &.current {
-        display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgb(92,144,182);
+  background: radial-gradient(circle, rgba(92,144,182,1) 0%, rgba(3,25,65,1) 100%);
+  font-family: $titleFont;
+
+  .container{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: white;
+    max-width: 75%;
+    min-height: 100%;
+    margin: auto;
+
+    h1 {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 70px;
     }
 
-    width: 100vw;
-    height: 100vh;
-    background: rgb(92,144,182);
-    background: radial-gradient(circle, rgba(92,144,182,1) 0%, rgba(3,25,65,1) 100%);
-    font-family: $titleFont;
-
-    .container{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        color: white;
-        max-width: 75%;
-        min-height: 100%;
-        margin: auto;
-
-        h1 {
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-size: 70px;
-        }
-
-        img {
-            width: 30%;
-        }
-
-
+    img {
+      width: 30%;
     }
+
+
+  }
 }
 
 
