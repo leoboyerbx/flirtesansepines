14b0c4ea520c5eca176f6bae794f26827e29ac8a
leoboyerbx <contact@leoboyer.fr>
2020-12-07 14:08:21 +0100

    fix error global var

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import store from './store'
 import variables from "@/variables";
 
-// Vue.prototype.$globals = variables
+Vue.prototype.$globals = variables
 
 Vue.config.productionTip = false
 
