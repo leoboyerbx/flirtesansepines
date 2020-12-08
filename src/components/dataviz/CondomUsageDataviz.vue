fc26d711caa99df60509e86de9a266c111a9a499
Maeva Reynaud <maeva.reynaud@hotmail.fr>
2020-12-08 10:02:42 +0100

    Advancement pie chart style and tooltip

--- src/components/dataviz/CondomUsageDataviz.vue
+++ src/components/dataviz/CondomUsageDataviz.vue
@@ -1,7 +1,15 @@
 <template>
-  <svg id="condom-usage-dataviz" width="500" height="500">
-  </svg>
-  
+  <div class="condom-usage-dataviz-container">
+    
+    <svg id="condom-usage-dataviz" width="500" height="500">
+    </svg>
+    <div id="tooltip">
+      <span class="arrow"></span>
+      <span class="value">{{ tooltipValues.percentage }} %</span>
+      <span class="desc">{{ tooltipValues.tooltip }}</span>
+      <button class="read-more"> En savoir +</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,7 +22,7 @@ export default {
     tooltipVisible: false,
     tooltipValues: {
       percentage: 0,
-      key: '',
+      tooltip: '',
       x: 0,
       y: 0
     }
@@ -22,9 +30,12 @@ export default {
   mounted() {
     this.generatePieChart();
   },
+  computed: {
+    colors () { return this.$globals.dataColors },
+  },
   methods: {
      generatePieChart() {
-       const data = [{name: 'Oui', value: 41.2},{name: 'Non', value: 17.1}, {name: 'Ayant déjà eu des rapports non protégés', value: 41.7}];
+       const data = [{name: 'Oui', value: 41.2, tooltip:"Des personnes interogées assurent utiliser le préservatif à chaque rapport sexuel"},{name: 'Non', value: 17.1, tooltip:"Des personnes interogées n'utilisent pas le préservatif pour leur rapport sexuel"}, {name: 'Ayant déjà eu des rapports non protégés', value: 41.7, tooltip:"Des personnes interogée n'utilisent pas sytèmatiquement le préservatif"}];
 
        const svg = d3.select('#condom-usage-dataviz'),
              width = svg.attr('width'),
@@ -33,8 +44,8 @@ export default {
         const radius = 200;
 
         const g = svg.append('g').attr('transform', `translate(${width/2}, ${height/2})`);
-
-        const color = d3.scaleOrdinal(['red','blue','green']);
+      console.log(this.$globals.dataColors[0])
+        const color = d3.scaleOrdinal(this.$globals.dataColors[0],this.$globals.dataColors[1],this.$globals.dataColors[2]);
 
         const csv = d3.scaleOrdinal(['datas/detailsCondomUsage.csv','datas/detailsNoCondomUsage.csv','']);
         const tooltip = d3.select("#condom-usage-dataviz").append("div")	
@@ -45,13 +56,11 @@ export default {
       
 
         const path = d3.arc().outerRadius(radius).innerRadius(0);
-        const label =d3.arc().outerRadius(radius).innerRadius(radius - 150);
 
         const pies = g.selectAll('.arc').data(pie(data)).enter().append('g').attr('class', 'arc').attr('csv', d => csv(d.data.value))
           .on("click",function(d) { 
               // The amount we need to rotate:
-              var rotate = 45-(d.explicitOriginalTarget.__data__.startAngle + d.explicitOriginalTarget.__data__.endAngle)/2 / Math.PI * 180;
-              // Transition the pie chart
+              var rotate = 45-(d.explicitOriginalTarget.__data__.startAngle + d.explicitOriginalTarget.__data__.endAngle)/2 / Math.PI * 180;              // Transition the pie chart
               g.transition()
                 .attr("transform",  "translate(" + width / 2 + "," + height / 2 + ") rotate(" + rotate + ")")
                 .duration(1000);
@@ -59,14 +68,20 @@ export default {
                   .attr("transform",  "translate(" + -width*1.4 + "," +  height / 2 + ")")
                   .duration(1000);
           })
-          .on('mouseover', () => {
-            this.tooltipVisible = true
+          .on('mouseover', (e, d) => {
+
+            d3.select("#tooltip")
+              .style("opacity", 1)
+            this.tooltipValues = {
+              percentage: (d.data.value),
+              tooltip: (d.data.tooltip)
+            }
+
           })
 
         pies.append('path').attr('d', path).attr('fill', d => color(d.data.value));
-        pies.append('text')
-            .text(d => d.data.name)
-            .attr('transform', d => `translate(${label.centroid(d)})`)
+
+        
 
   }
 }
@@ -76,11 +91,61 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-#condom-usage-dataviz {
-  position:relative;
-  .arc{
-    text{
-      text-anchor: middle;
+
+.condom-usage-dataviz-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  #tooltip {
+    background-color: white;
+    max-width: 200px;
+    display: flex;
+    flex-direction:column;
+    text-align: center;
+    font-family: $titleFont;
+    height: fit-content;
+    position: relative;
+    padding: 1.2rem;
+    .value {
+      color: $themeRed;
+      font-size: 1.9rem;
+      margin-bottom: 1rem;
+    }
+
+    .desc {
+      color: black;
+      font-size: 1.2rem;
+      margin-bottom: 2.3rem;
+    }
+
+    button {
+      background-color: $themeBlue2;
+      color: #ffff;
+      border-radius: 50px;
+      padding: 15px 20px;
+      font-family: $paragraphFont;
+      font-size: 1.2rem;
+      border:none;
+    }
+
+    .arrow {
+      width: 0;
+      height: 0;
+      border-left: 20px solid transparent;
+      border-right: 20px solid transparent;
+      border-top: 20px solid #FFFF;
+      position: absolute;
+      top: 43%;
+      left: -30px;
+      transform: rotate(90deg);
+    }
+      
+  }
+  #condom-usage-dataviz {
+    .arc{
+      text{
+        text-anchor: middle;
+      }
     }
   }
 }
