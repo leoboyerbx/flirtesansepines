499f0bcd56edee8a7f4b6221ebaf162b50509ad3
leoboyerbx <contact@leoboyer.fr>
2020-12-08 15:44:34 +0100

    WIP On lottie animations

--- src/components/slides/SeropositivyEstimationSequence.vue
+++ src/components/slides/SeropositivyEstimationSequence.vue
@@ -1,20 +1,27 @@
 <template>
   <article class="seropositivity-estimation" :class="currentState">
-    <div class="container">
-      <h1>{{ msg }}</h1>
-      <p class="estimation-number">{{ number}}</p>
-      <input class="slider" type="range" min="1000" max="10000" step="1000" v-model="number">
-      <button @click="submitEstimation"> Valider </button>
-    </div>
+  <lottie-animation
+    path="lottie/pullFlower.json"
+    @AnimControl="setAnimcontroller"
+  />
   </article>
 </template>
 
 <script>
+import * as animationData from '@/assets/lottie/pullFlower.json'
+import LottieAnimation from "lottie-vuejs/src/LottieAnimation.vue";
+
 export default {
   name: 'SeropositivityEstimationSequence',
+  components: {
+      LottieAnimation
+  },
   data: () => ({
     msg: "A votre avis, combien de cas de séropositivité ont été découverts en 2019 en France ?",
-    number:1000
+    number:1000,
+    defaultOptions: {animationData: animationData},
+    animationSpeed: 1,
+    anim: null
   }),
   props: {
     currentState: {
@@ -23,9 +30,9 @@ export default {
     }
   },
   methods: {
-    submitEstimation() {
-      this.userEstimation = this.number;
-      this.$emit('next-slide');
+    setAnimcontroller (anim) {
+      this.anim = anim
+      console.log(this.anim.animationData.markers)
     }
   }
 }
@@ -35,93 +42,10 @@ export default {
 <style scoped lang="scss">
 
 .seropositivity-estimation {
-  display:none;
-  position: fixed;
-  top: 0;
+  position: absolute;
   left: 0;
-  width:100%;
-  height: 100%;
-  background-color: $backgroundColor;
-
-  &.current {
-    display: block;
-  }
-
-  .container{
-    max-width: 80%;
-    margin:auto;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 60px 0;
-    h1 {
-      font-family: $titleFont;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 50px;
-      color: white;
-      margin:0;
-      color: $themeBlue3;
-      text-align: center;
-    }
-
-
-    img {
-      width: 30px;
-    }
-
-    .estimation-number {
-      border:none;
-      outline:none;
-      font-family:$paragraphFont;
-      font-size: 200px;
-      color: white;
-      font-weight: 700;
-      display: flex;
-      margin: auto;
-    }
-
-    .slider {
-
-      max-width: 50%;
-      margin: auto !important;
-      -webkit-appearance: none;
-      margin: 10px 0;
-      width: 100%;
-
-      &:hover, &:focus {
-        outline:none;
-      }
-
-      &::-moz-range-thumb {
-        width: 160px;
-        height: 160px;
-        border: 0;
-        background: url('../../assets/cursor.svg');
-        cursor: pointer;
-      }
-
-      &::-moz-range-track {
-        width: 100%;
-        cursor: pointer;
-        //box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
-        background: url('../../assets/track.svg');
-        background-size: contain;
-        background-position: 50%;
-        background-repeat: no-repeat;
-        height: 40px;
-        border-radius: 25px;
-        border: 0px solid #000101;
-      }
-    }
-  }
-
-
-
-
-
+  bottom: 0;
+  //background-color: #ff0000;
 }
 
 </style>
