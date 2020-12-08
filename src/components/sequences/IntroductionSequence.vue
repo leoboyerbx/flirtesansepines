6174c0a90436806724fbfe802ae13308c9fd002a
leoboyerbx <contact@leoboyer.fr>
2020-12-08 21:42:44 +0100

    🚧 WIP Sequence estimation

--- src/components/sequences/IntroductionSequence.vue
+++ src/components/sequences/IntroductionSequence.vue
@@ -1,27 +1,44 @@
 <template>
-  <article :style="{transform: transformProprety }" v-on:wheel="onWheel" class="experience-introduction" :class="currentState">
-    <div class="lottie-wrapper">
+  <article
+      :style="{transform: transformProprety }"
+      v-on:wheel="onWheel"
+      class="experience-introduction"
+      :class="currentState"
+  >
+    <section class="lottie-wrapper">
       <lottieAnimation
           class="intro-lottie"
           path="lottie/introAnim.json"
-          :auto-play="true"
+          :auto-play="false"
+          :loop="false"
           @AnimControl="setAnimController"
+          :speed="0.4"
       />
+    </section>
+    <div class="title" :class="{ visible: titleVisible }">
+      <h1>Flirt sans épines</h1>
     </div>
+    <HIVEstimation class="hiv-estimation" />
   </article>
 </template>
 
 <script>
 import LottieAnimation from "@/components/lib/LottieAnimation";
+import HIVEstimation from "@/components/sequences/partials/HIVEstimation";
 
 export default {
   name: 'IntroductionSequence',
   components: {
+    HIVEstimation,
     LottieAnimation
   },
   data: () => ({
     translateY: 0,
-    scrollFactor: 10
+    scrollFactor: 10,
+    lottieFactor: 1,
+    lottiePlaying: false,
+    isScrollAnimating: false,
+    titleVisible: false
   }),
   props: {
     currentState: {
@@ -35,26 +52,83 @@ export default {
           ? 'translate3d(0, -100%, 0)'
           : 'translate3d(0, ' + this.translateY + 'px ,0)'
     },
+    totalFrames () {
+      return this.anim.totalFrames || 0
+    }
   },
   methods: {
     onWheel (e) {
-      const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-      const scrollLimit = this.$el.offsetHeight - vh
+      if (this.anim.currentFrame < this.totalFrames -1) {
+        this.handleWheelLottie(e)
+      } else {
+        this.handleWheelScroll(e)
+      }
+    },
+    handleWheelScroll (e) {
+      if (!this.isScrollAnimating) {
+        const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+        const scrollLimit = this.$el.offsetHeight - vh
 
-      const canGoUp = e.deltaY < 0 && this.translateY < 0
-      const canGoDown = e.deltaY > 0 && this.translateY > -scrollLimit
-      if (canGoUp || canGoDown) {
-        this.translateY -= e.deltaY * this.scrollFactor
-      } else if (e.deltaY > 0 && this.translateY <= -scrollLimit && !this.displayNextSlide) {
-        if (this.currentState !== 'past') {
-          this.$emit('next-slide')
+        const canGoUp = e.deltaY < 0 && this.translateY < 0
+        const canGoDown = e.deltaY > 0 && this.translateY > -scrollLimit
+        if (canGoUp || canGoDown) {
+          this.translateY -= e.deltaY * this.scrollFactor
+          this.isScrollAnimating = true
+          setTimeout(() => {
+            this.isScrollAnimating = false
+          }, 400)
+        } else if (e.deltaY > 0 && this.translateY <= -scrollLimit && !this.displayNextSlide) {
+          if (this.currentState !== 'past') {
+            this.$emit('next-slide')
+          }
         }
-
       }
       if (this.translateY > 0) this.translateY = 0
     },
+    handleWheelLottie (e) {
+      if (!this.lottiePlaying && e.deltaY > 0) {
+        this.anim.play()
+        this.lottiePlaying = true
+
+        if (this.anim.currentFrame < 90) {
+          setTimeout(() => {
+            this.anim.pause()
+            this.lottiePlaying = false
+          }, 400)
+        } else {
+          this.anim.setSpeed(1)
+        }
+      }
+
+      // if (e.deltaY > 0) {
+      //   this.anim.goToAndStop(this.anim.currentFrame + 1, true)
+      // }
+      // if (e.deltaY > 0) {
+      //   console.log(this.anim.currentFrame)
+      //   this.anim.playSegments([
+      //     [this.anim.currentSlide, this.anim.currentSlide + e.deltaY * this.lottieFactor]
+      //   ], false)
+      // }
+    },
     setAnimController (anim) {
       this.anim = anim
+      this.anim.goToAndStop(50, true)
+      window.anim = anim
+      this.anim.addEventListener('complete', () => {
+        this.titleVisible = true
+      })
+      // window.setInterval(() => {
+      //   console.log(this.anim.currentFrame)
+      // }, 10)
+    },
+    getMarkerByKey (key) {
+      if (this.anim.animationData.markers) {
+        for (const marker of this.anim.animationData.markers) {
+          if (marker.cm === key) return marker
+        }
+        return null
+      }
+      return null
     }
   }
 }
@@ -68,25 +142,55 @@ export default {
   left: 0;
   width: 100%;
   min-height: 100%;
-  transition: all .5s;
+  transition: all .8s ease;
 
   .lottie-wrapper {
-    position: absolute;
-    top: 0;
-    left: 0;
     width: 100%;
-    height: 100vh;
-    background-color: $themeRed;
+    //background-color: $themeRed;
     .intro-lottie {
-      position: absolute;
-      left: 0;
-      bottom: -200px;
+      position: relative;
+      //left: 0;
+      //top: 0;
       width: 100%;
-      max-height: 100vh;
+      //max-height: 100vh;
       svg {
         height: auto;
       }
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: -12px;
+        height: 20px;
+        left: 0;
+        width: 100%;
+        background-color: #fff;
+      }
     }
+  }
+  .title {
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 100vh;
+    width: 50%;
+    display: flex;
+    align-items: center;
+    transition: all .8s;
+    //transform: translateY(30px);
+    opacity: 0;
+    h1 {
+      font-size: 4rem;
+      color: #fff;
+      font-family: $titleFont;
+      margin: 0;
+    }
+    &.visible {
+      opacity: 1;
+      //transform: translateY(0);
+    }
+  }
+  .hiv-estimation {
+    transform: translateY(-150px);
   }
 }
 </style>
---
+++ src/components/sequences/partials/HIVEstimation.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="hiv-estimation">
+    <p class="question">À votre avis, combien de cas de séropositivité ont été découverts en 2019 en France ?</p>
+    <div class="number">{{ displayNumber }}</div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: "HIVEstimation",
+  data: () => ({
+    number: 0
+  }),
+  computed: {
+    displayNumber () {
+      let res = '' + this.number;
+      while (res.length < 5) {
+        res = '0' + res;
+      }
+      return res;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.hiv-estimation {
+  position: relative;
+  height: 80vh;
+  width: 100%;
+  .question {
+    color: $themeBlue3;
+    font-family: $titleFont;
+    font-size: 3.5rem;
+    width: 100%;
+    text-align: center;
+    padding: 0 20%;
+    margin: 0;
+  }
+  .number {
+    padding: 0 10px;
+    font-weight: bold;
+    color: $themeRed;
+    font-size: 20rem;
+    text-align: right;
+    line-height: 20rem;
+  }
+}
+</style>
