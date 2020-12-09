5ff202452cf09aeb3cf0c167ac2f1c2128557849
leoboyerbx <contact@leoboyer.fr>
2020-12-09 19:53:53 +0100

    ✨ WIP Screening Late sequence

--- src/components/sequences/ScreeningLateSequence.vue
+++ src/components/sequences/ScreeningLateSequence.vue
@@ -1,6 +1,6 @@
 <template>
   <article
-      v-on:wheel="onWheel"
+      v-on:wheel="onWheelChangeSlide"
       class="screening-late"
       :class="[ currentState, arrivingClass ]"
       :style="{ display: displayStyle }"
@@ -14,10 +14,12 @@
 
 <script>
 import ScreeningLateDataviz from "@/components/dataviz/ScreeningLateDataviz";
+import sequence from "@/mixins/sequenceMixin";
 
 export default {
   name: 'ScreeningLateSequence',
   components: {ScreeningLateDataviz},
+  mixins: [ sequence ],
   data: () => ({
     viewMode: 0,
     displayNextSlide: false,
@@ -30,27 +32,6 @@ export default {
       default: 'future'
     }
   },
-  methods: {
-    onWheel (e) {
-      const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-      const scrollLimit = this.$el.offsetHeight - vh
-
-      const canGoUp = e.deltaY < 0 && this.translateY < 0
-      const canGoDown = e.deltaY > 0 && this.translateY > -scrollLimit
-      console.log(e.deltaY)
-      console.log(this.translateY)
-      if (canGoUp || canGoDown) {
-        this.translateY -= e.deltaY * this.scrollFactor
-      } else if (e.deltaY > 0 && this.translateY <= -scrollLimit && !this.displayNextSlide) {
-        this.$emit('next-slide');
-        //document.getElementById('zoomable').getElementsByClassName('.checkmark');
-        this.displayNextSlide = true;
-      }
-    }
-  },
-  computed: {
-
-  }
 }
 </script>
 
@@ -58,22 +39,21 @@ export default {
 <style scoped lang="scss">
 
 .screening-late {
-    top: 0;
+  top: 0;
   left: 0;
   width: 100%;
   min-height: 100%;
-  transition: all .5s;
-  opacity:0;
-  visibility: hidden;
-  transition-delay: .5s;
+  transition: all $slideDurationEasing;
   margin: auto;
   padding: 4% 10%;
 
-  &.current {
-    position: fixed;
-    opacity:1;
-    visibility: visible;
+  &.future {
+    opacity: 0;
   }
+  &.arriving-forward {
+    animation: fade $slideDurationEasing;
+  }
+
 
   h1 {
     color: $themeBlue3;
